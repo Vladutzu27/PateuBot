3463836e70faba30f7f0f4df70bd55db5e5a109c
Vlad-Andrei Medințu <80452570+Vladutzu27@users.noreply.github.com>
2022-09-23 17:57:06 +0300

    Am băgat butoane

--- functions/events/discord/command/brainly.js
+++ functions/events/discord/command/brainly.js
@@ -1,14 +1,42 @@
-//this one works
-
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const event = context.params.event;
 const question = event.data.options[0].value;
 const formatedQuestion = encodeURIComponent(question);
 
+
 await lib.discord.channels['@0.3.0'].messages.create({
   "channel_id": `${context.params.event.channel_id}`,
   "content": "",
   "tts": false,
+  "components": [
+    {
+      "type": 1,
+      "components": [
+        {
+          "style": 5,
+          "label": `Mai multe resurse de învățare`,
+          "url": `https://clasa8ablog.wixsite.com/clasa-8a/post/mai-multe-resurse-folositoare-pentru-educa%C8%9Bie`,
+          "disabled": false,
+          "emoji": {
+            "id": null,
+            "name": `💻`
+          },
+          "type": 2
+        },
+        {
+          "style": 5,
+          "label": `Descarca toat manualele in format PDF`,
+          "url": `https://easyupload.io/ydpiji`,
+          "disabled": false,
+          "emoji": {
+            "id": null,
+            "name": `📚`
+          },
+          "type": 2
+        }
+      ]
+    }
+  ],
   "embeds": [
     {
       "type": "rich",
@@ -24,3 +52,4 @@ await lib.discord.channels['@0.3.0'].messages.create({
     }
   ]
 });
+
